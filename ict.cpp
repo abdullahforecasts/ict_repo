40f38c160b0463a5ed57f97eca038a5bf9045611
abdullahforecasts <abdullahforecasts@gmail.com>
2024-10-10 09:34:57 +0500

    reply to cpp hello

--- ict.cpp
+++ ict.cpp
@@ -2,7 +2,8 @@
 #include <conio.h>
 using namespace std;
 int main(){
-    cout<<"hello";
+    cout<<"hello"<<'\n';
+    cout<<"have a nice day with a fresh restart!";
     _getch();
     return 0;
 }
